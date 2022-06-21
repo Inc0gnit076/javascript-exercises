2e3f22d7c030edc845841a803994886f898c21de
Inc0gnit076 <kmacinnis76@gmail.com>
2022-06-21 12:52:40 -0400

    complete exercise 2 all tests pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function( word , n) { 
+    let answer = [];
+    if (n < 0) {
+        return ("ERROR");
+    }
+    else {
+    for (let i = 0 ; i < n; i++) {
+        answer.push(word);
+    }
+    answer = answer.join("");
+    return (answer);
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
