214af914d30ea264bad27eeb29902fb13920d8d6
Inc0gnit076 <kmacinnis76@gmail.com>
2022-06-24 09:43:18 -0400

    complete exercise 7 and passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let celsius = (temp - 32) * (5/9);
+  return parseFloat(celsius.toFixed(1));
 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let f = temp * 9 / 5 + 32;
+  return parseFloat(f.toFixed(1));
 
 };
 
