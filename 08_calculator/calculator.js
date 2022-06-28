31293e4358c8ffe458ad9a2e4b7469f6e03a44a2
Inc0gnit076 <kmacinnis76@gmail.com>
2022-06-28 06:45:08 -0400

    complete exercise 08 and passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
+const add = function(a , b) { 
+	  return a + b
+};
+
+const subtract = function(a , b) { 
+	  return a - b
+};
+
+const sum = function(array) {
+  let c = 0
+	array.forEach(element => { c += element;
+    
+  });
+  return c
+  };
+
+const multiply = function(array) {
+  let c = 1
+	array.forEach(element => { c *= element;
+    
+  });
+  return c
+
+};
+
+const power = function(a , b) { return a **b;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(a) {
+  if (a == 0){
+    return 1
+  }
+  else {
+    return a * factorial(a-1);
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  }
 	
 };
 
